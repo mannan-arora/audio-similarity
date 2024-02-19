28e1d5ce8053023ef99d995bdca160a316208d78
Mannan Arora <m43arora@uwaterloo.ca>
2024-02-19 13:59:59 -0500

    Implement audio file loading

--- similarity.cc
+++ similarity.cc
@@ -1,1 +1,23 @@
 #include "similarity.h"
+
+Similarity::Similarity(std::string originalFilePath, std::string compareFilePath, float sampleRate) {
+    this->originalFilePath = originalFilePath;
+    this->compareFilePath = compareFilePath;
+    this->sampleRate = sampleRate;
+
+    loadAudioFiles();
+}
+
+bool Similarity::loadAudioFiles() {
+    originalFile.load(originalFilePath);
+    compareFile.load(compareFilePath);
+
+    if (originalFile.getSampleRate() != sampleRate) {
+        originalFile.setSampleRate(sampleRate);
+    }
+    if (compareFile.getSampleRate() != sampleRate) {
+        compareFile.setSampleRate(sampleRate);
+    }
+
+    return true;
+}
