d2006c4324ff22f273d93ca051c45e70a6278f0b
Mannan Arora <m43arora@uwaterloo.ca>
2024-02-19 13:59:31 -0500

    Define similarity header

--- similarity.h
+++ similarity.h
@@ -1,9 +1,29 @@
 #ifndef _SIMILARITY_H_
 #define _SIMILARITY_H_
 
+#include "AudioFile.h"
 #include <string>
 
 class Similarity {
+private:
+    std::string originalFilePath;
+    std::string compareFilePath;
+    float sampleRate;
+
+    AudioFile<float> originalFile;
+    AudioFile<float> compareFile;
+
+public:
+    bool loadAudioFiles();
+
+    float zcrSimilairty() const;
+    float rhythmSimilarity() const;
+    float chromaSimilarity() const;
+    float energyEnvelopeSimilarity() const;
+    float spectralContrastSimilarity() const;
+    float perceptualSimilarity() const;
+
+    Similarity(std::string originalFilePath, std::string compareFilePath, float sampleRate = 44100.f);
 };
 
 #endif
